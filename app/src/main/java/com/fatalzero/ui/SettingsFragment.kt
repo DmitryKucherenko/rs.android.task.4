87999738457ca42228d247e05076dc42e23df37c
Dmitry <fatalzero@mail.ru>
2021-09-14 22:46:06 +0300

    refact: settings preference replace cod with xml settings

--- app/src/main/java/com/fatalzero/ui/SettingsFragment.kt
+++ app/src/main/java/com/fatalzero/ui/SettingsFragment.kt
@@ -1,48 +1,16 @@
 package com.fatalzero.ui
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
-import androidx.preference.SwitchPreference
 import com.fatalzero.R
 
-private const val SORT_BY_KEY = "sort_by"
-private const val SWITCH_KEY = "switch"
-private const val ROOM = "Room"
-private const val CURSOR = "Cursor"
-private const val BRAND_SORT = "brand"
 
 
-class SettingsFragment : PreferenceFragmentCompat(),
-    SharedPreferences.OnSharedPreferenceChangeListener {
-    var sharedPreferences: SharedPreferences? = null
-    var listPreference: ListPreference? = null
-    var switchPreference: SwitchPreference? = null
+class SettingsFragment : PreferenceFragmentCompat(){
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
-        listPreference = findPreference<ListPreference>(SORT_BY_KEY)
-        switchPreference = findPreference<SwitchPreference>(SWITCH_KEY)
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
-        listPreference?.summary =
-            getString(R.string.sort_by_word) + " " + sharedPreferences?.getString(SORT_BY_KEY, "")
-        switchPreference?.summary =
-            if (sharedPreferences?.getBoolean(SWITCH_KEY, true) == true) ROOM else CURSOR
-
     }
 
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        when (key) {
-            SORT_BY_KEY -> listPreference?.summary =
-                getString(R.string.sort_by_word)+" " + sharedPreferences?.getString(SORT_BY_KEY, BRAND_SORT)
-            SWITCH_KEY -> switchPreference?.summary =
-                if (sharedPreferences?.getBoolean(SWITCH_KEY, true) == true) ROOM else CURSOR
-
-        }
-    }
 
 }
